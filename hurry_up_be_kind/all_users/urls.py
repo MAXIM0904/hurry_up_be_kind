ca62e9a2c369b4f4d95f898b9ed5695e4ab77a54
Max <dvdvdvd90@mail.ru>
2022-04-18 15:39:47 +0300

    ar_work 1_2

--- hurry_up_be_kind/all_users/urls.py
+++
@@ -1,6 +0,0 @@
-from .views import AllPhilantropistListView
-from django.urls import path
-
-urlpatterns = [
-    path('', AllPhilantropistListView.as_view(), name='all_philantropist'),
-]
--- hurry_up_be_kind/all_users/views.py
+++
@@ -1,7 +0,0 @@
-from django.views.generic import ListView
-from .models import Philantropist, Ward
-
-class AllPhilantropistListView(ListView):
-    model = Ward
-    template_name = 'forms_html/users_list.html'
-
